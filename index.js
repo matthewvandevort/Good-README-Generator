255135a47a1cc864886250aa9e1816d8a23df8fd
Matthew Vandevort <vandevortmatthew@gmail.com>
2021-08-01 16:48:16 -0400

    added constants for "require"

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+console.log('Hello! Time to create a README.md file for your project!');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: 'title',
+        message:'Please enter the title of your project.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
